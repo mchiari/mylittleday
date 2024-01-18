cb8bab1fcb4b830ffe8c17a0919f26c634d1d4cc
chiarimatheus <mchiaridev@gmail.com>
2024-01-17 22:28:26 -0300

    fix update user error

--- client/src/app/(pages)/user/page.tsx
+++ client/src/app/(pages)/user/page.tsx
@@ -13,7 +13,7 @@ import React, { Suspense, useEffect } from "react";
 import { useQuery } from "react-query";
 import { User } from "./types";
 import { Button } from "@/components/ui/button";
-import { Pencil1Icon, Pencil2Icon } from "@radix-ui/react-icons";
+import { CheckIcon, Pencil1Icon, Pencil2Icon } from "@radix-ui/react-icons";
 import {
   Dialog,
   DialogContent,
@@ -35,6 +35,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import axios from "axios";
+import { toast } from "sonner";
 
 const Users = () => {
   const { isLoading, error, data } = useQuery("getUsers", () =>
@@ -78,7 +79,8 @@ const Users = () => {
 const UserEditor = ({ user }: { user: User }) => {
   const formSchema = z.object({
     name: z.string().max(50),
-    cpf: z.string().max(11)
+    cpf: z.string().max(11),
+    _id: z.string(),
     // password: z.string().min(2).max(50),
     // confirmPassword: z.string().min(2).max(50),
   });
@@ -87,7 +89,8 @@ const UserEditor = ({ user }: { user: User }) => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: user.name,
-      cpf: user.cpf
+      cpf: user.cpf,
+      _id: user._id,
       // password: "",
       // confirmPassword: "",
     },
@@ -95,14 +98,28 @@ const UserEditor = ({ user }: { user: User }) => {
 
   const handleSubmit = (values: z.infer<typeof formSchema>) => {
     const request = axios
-      .put(process.env.NEXT_PUBLIC_SERVER_BASE_PATH + "users/"+user._id, values, {
-        withCredentials: true,
-      })
+      .put(
+        process.env.NEXT_PUBLIC_SERVER_BASE_PATH + "users/" + user._id,
+        values,
+        {
+          withCredentials: true,
+        }
+      )
       .then((res) => {
-        console.log(res)
+        console.log(res);
+        if (res.status === 200) {
+          toast.success("Atualização de dados", {
+            description: `Dados de ${user.name} atualizados com sucesso`,
+            // icon: <CheckIcon />,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Atualização de dados", {
+          description: error.message,
+        });
       });
-
-    //TODO: DEBUG UPDATE USER 
   };
 
   return (
@@ -114,35 +131,38 @@ const UserEditor = ({ user }: { user: User }) => {
         <DialogHeader>
           <DialogTitle>Edit user</DialogTitle>
           <DialogDescription>
-              <Form {...form}>
-                <form onSubmit={form.handleSubmit(handleSubmit)} className="flex flex-col gap-4 mt-4 justify-center items-center w-full">
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem className="w-3/4">
-                        <FormLabel>Name</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Insert e-mail" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="cpf"
-                    render={({ field }) => (
-                      <FormItem className="w-3/4">
-                        <FormLabel>CPF</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Insert cpf" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  {/* <FormField
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(handleSubmit)}
+                className="flex flex-col gap-4 mt-4 justify-center items-center w-full"
+              >
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="w-3/4">
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Insert e-mail" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="cpf"
+                  render={({ field }) => (
+                    <FormItem className="w-3/4">
+                      <FormLabel>CPF</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Insert cpf" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* <FormField
                     control={form.control}
                     name="password"
                     render={({ field }) => (
@@ -169,15 +189,15 @@ const UserEditor = ({ user }: { user: User }) => {
                     )}
                   /> */}
 
-                  <Button
-                    disabled={!form.formState.isValid}
-                    type="submit"
-                    className="w-3/4"
-                  >
-                    Login
-                  </Button>
-                </form>
-              </Form>
+                <Button
+                  disabled={!form.formState.isValid}
+                  type="submit"
+                  className="w-3/4"
+                >
+                  Atualizar dados
+                </Button>
+              </form>
+            </Form>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
